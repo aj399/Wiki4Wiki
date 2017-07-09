79f0ad06ae2a1837f770c4cc1ff7476310d5b370
Anand Joseph <amarattukalam@gmail.com>
2017-07-09 14:16:38 -0400

    Commented RedisSink.java

--- Streaming/kafka_Flink/src/main/java/consumer/RedisSinks.java
+++ Streaming/kafka_Flink/src/main/java/consumer/RedisSinks.java
@@ -1,3 +1,5 @@
+//All the redis sinks used in MainStream.java
+
 package consumer;
 
 import java.util.*;
@@ -9,25 +11,27 @@ import org.apache.flink.streaming.connectors.redis.common.mapper.RedisMapper;
 
 
 public class RedisSinks{
-
+	
+	//Redis mapper to send the trending articles to redis list keyed by 'trending'
 	public static class TrendingRedisMapper implements RedisMapper<Tuple8<String, String, Long, Long, Long, Long, Long, Long>>{
 
-        @Override
-        public RedisCommandDescription getCommandDescription() {
-            return new RedisCommandDescription(RedisCommand.LPUSH, "TRENDING");
-        }
+		@Override
+		public RedisCommandDescription getCommandDescription() {
+		    return new RedisCommandDescription(RedisCommand.LPUSH, "TRENDING");
+		}
 
-        @Override
-        public String getKeyFromData(Tuple8<String, String, Long, Long, Long, Long, Long, Long> data) {
-            return (String) "trending";
-        }
+		@Override
+		public String getKeyFromData(Tuple8<String, String, Long, Long, Long, Long, Long, Long> data) {
+		    return (String) "trending";
+		}
 
-        @Override
-        public String getValueFromData(Tuple8<String, String, Long, Long, Long, Long, Long, Long> data) {
-            return  data.f0+"\t"+data.f1+"\t"+data.f2+":"+data.f3+":"+data.f4+":"+data.f5+":"+data.f6+":"+data.f7;
-        }
-    }
+		@Override
+		public String getValueFromData(Tuple8<String, String, Long, Long, Long, Long, Long, Long> data) {
+		    return  data.f0+"\t"+data.f1+"\t"+data.f2+":"+data.f3+":"+data.f4+":"+data.f5+":"+data.f6+":"+data.f7;
+		}
+	}
 	
+	//Redis mapper to send the hot articles to redis list keyed by 'Hot Topic'(currently un used)
 	public static class HotRedisMapper implements RedisMapper<Tuple3<String, String, Long>>{
 
         @Override
