6f32908765d23caee347d85ec1ab7140ec5577a3
Anand Joseph <amarattukalam@gmail.com>
2017-07-09 11:23:55 -0400

    Update canberra.py

--- Batch/PySpark/canberra.py
+++ Batch/PySpark/canberra.py
@@ -1,3 +1,10 @@
+//Calculates canberra distance of the current pageview and and historical average by representing both current pageview and historical averages as vectors
+//cPi = current pageview of article at hour i
+//aPi = historical average pageview of article at hour i
+//vCPi = vector representation of current pageview of article at hour i = (cP0,cP1,..,cPi)
+//vAPi = vector representation of historical average pageview of article at hour i = (aP0 ,aP1 ,.. ,aPi)
+//Difference in current pageview pattern of the current day and historical data(calculated by canberra distance) = ``$ y=\sum_{i=1}^n g(x_i) $``
+
 from pyspark import SparkContext, SparkConf
 import json
 import datetime
@@ -9,8 +16,9 @@ currHr = datetime.datetime.now().hour
 sCurrHr = str(currHr)
 if currHr<10:
 	sCurrHr = "0"+sCurrHr
-	
-currFileName = str(datetime.datetime.now().year)+str(datetime.datetime.now().month)+str(datetime.datetime.now().day)+str(datetime.datetime.now().hour)
+
+//File naming convention of the op file : 'yyyy:mm:dd:hr'
+currFileName = str(datetime.datetime.now().year)+":"+str(datetime.datetime.now().month)+":"+str(datetime.datetime.now().day)+":"+str(datetime.datetime.now().hour)
 
 with open('../batchConfig.json') as config_file:    
     config = json.load(config_file)
@@ -24,8 +32,11 @@ WKFOLDER = config["WORK_FOLDER_NAME"]
 TMFOLDER = config["TEMP_FOLDER_NAME"]
 REDIS = config["REDIS_IP"]
 
-conn = redis.StrictRedis(host=REDIS, port=6379, encoding='utf-8')
+conn = redis.StrictRedis(host=REDIS, port=6379, encoding='utf-8') // redis connector
+//Hourly aggregation of page requests send by flink (currently files are arriving not in time bound manner, thats why anomaly detection is disabled), probably should use apache airlow
+//instead of cron to trigger the spark job						
 currHrPageViewRdd = sc.textFile("s3a://"+ACCKEY+":"+SECKEY+"@"+BUCKET+"/"+WKFOLDER+"/"+sCurrHr).map(lambda x:x.split(" ")).map(lambda x:((x[1]).lower(),long(x[2]))).reduceByKey(add)
+//Historical average of page requests calculated by BatchAvgArtView
 histHrPageViewRdd = sc.sequenceFile("s3a://"+ACCKEY+":"+SECKEY+"@"+BUCKET+"/"+OPFOLDER+"/avg/"+sCurrHr)
 currDiffRdd = currHrPageViewRdd.join(histHrPageViewRdd).map(lambda x:(x[0], (abs(x[1][0]-x[1][1]), x[1][0]+x[1][1])))
 currDiffRdd.persist()
