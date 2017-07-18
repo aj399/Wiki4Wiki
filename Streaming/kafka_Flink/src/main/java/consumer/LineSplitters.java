9a344cf7659d7c063479f7546e46e9172a6a81e1
Anand Joseph <amarattukalam@gmail.com>
2017-07-17 20:38:32 -0400

    exception handling LineSplitters.java

--- Streaming/kafka_Flink/src/main/java/consumer/LineSplitters.java
+++ Streaming/kafka_Flink/src/main/java/consumer/LineSplitters.java
@@ -11,21 +11,24 @@ import org.apache.flink.api.java.tuple.Tuple8;
 	//LineSplitter to extract Article name, timestamp and count(1) from input kafka stream, streaming live page request data created by pykafka_producer.py
 	public static class LnSplitArtTimePv implements FlatMapFunction<String, Tuple3<String, Long, Long >> {
 		@Override
-		public void flatMap(String line, Collector<Tuple3<String, Long, Long>> out) {
-		    String[] words = line.split("\t");
-		    out.collect(new Tuple3<String, Long, Long >(words[0], Long.parseLong(words[1]), 1L));
+		public void flatMap(String line, Collector<Tuple3<String, Long, Long>> out) throws NumberFormatException, IllegalArgumentException {
+			String[] words = line.split("\t");
+			if(isVaildIp(words[1]){
+				throw new IllegalArgumentException("Invalid Ip Address");
+			}
+			out.collect(new Tuple3<String, Long, Long >(words[0], Long.parseLong(words[2]), 1L)); //Not considering ip address(words[1]), as it is not useful for now
 		}
     	}
 	
 	//LineSplitter to extract page requests of 10 minute windows in the 1 hr period currently concatenated into a single string
-	public static class LnSplitArtPv1Hr6no implements FlatMapFunction<Tuple3<String, String, String >, Tuple8<String, String, Long, Long, Long, Long, Long, Long>> {
+	public static class LnSplitArtPv1Hr6no implements FlatMapFunction<Tuple3<String, String, String >, Tuple8<String, String, Long, Long, Long, Long, Long, Long>> throws IllegalArgumentException {
 		@Override
 		public void flatMap(Tuple3<String, String, String > line, Collector<Tuple8<String, String, Long, Long, Long, Long, Long, Long>> out) {
-		    String[] counts = line.f2.split("\t");
+			String[] counts = line.f2.split("\t");
 				if (counts.length<6){
-					counts = new String[]{"0","1","0","0","0", "0"};
+					throw new IllegalArgumentException("1 hour window not complete"); //If for any case flatmap function for 1 hr window gets called before the hour is complete or all six window values not recieved from kafka
 				}
-		    out.collect(new Tuple8<String, String, Long, Long, Long, Long, Long, Long>(line.f0, line.f1, Long.parseLong(counts[0]),Long.parseLong(counts[1]), Long.parseLong(counts[2]), Long.parseLong(counts[3]), Long.parseLong(counts[4]), Long.parseLong(counts[5])));
+			out.collect(new Tuple8<String, String, Long, Long, Long, Long, Long, Long>(line.f0, line.f1, Long.parseLong(counts[0]),Long.parseLong(counts[1]), Long.parseLong(counts[2]), Long.parseLong(counts[3]), Long.parseLong(counts[4]), Long.parseLong(counts[5])));
 		}
     	}
 	
@@ -33,8 +36,8 @@ import org.apache.flink.api.java.tuple.Tuple8;
 	public static class LnSplitArtTimePv1Min implements FlatMapFunction<String, Tuple3<String, String, String >> {
 		@Override
 		public void flatMap(String line, Collector<Tuple3<String, String, String>> out) {
-		    String[] words = line.split("\t");
-		    out.collect(new Tuple3<String, String, String >(words[0], words[1], words[2]));
+			String[] words = line.split("\t");
+			out.collect(new Tuple3<String, String, String >(words[0], words[1], words[2]));
 		}
     	}
 
